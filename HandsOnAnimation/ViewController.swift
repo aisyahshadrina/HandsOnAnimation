0bd73b059ca46ebbca5ebe9d2583df13511bf669
Aisyah Shadrina <aisyahshadrina@gmail.com>
2019-05-17 09:01:39 +0700

    Initial Commit

---
+++ HandsOnAnimation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HandsOnAnimation
+//
+//  Created by Aisyah Shadrina on 17/05/19.
+//  Copyright © 2019 Aisyah Shadrina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
